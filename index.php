e82c1bee31dac6cef8a1247f61f94e4d77b2da06
PR08 <PR08@figd-1611708.figdtk.de>
2017-02-21 13:39:16 +0100

    new testversion

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <title>Moonprojects</title>
+	<link href="css/r_styles.css" rel="stylesheet" type="text/css"/>
+	<script src="js/main.js" type="text/javascript"></script>
     </head>
     <body>
 	Git Testing
