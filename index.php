f7f643ab3e35e287b8559407ba08fe5e1b294c81
Dozent117 <Dozent117@figd-1611700.figdtk.de>
2017-02-21 13:08:57 +0100

    new project - little things changed

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <!--
-https://github.com/swnbckhs/moonproject.git
 
-swnbckhs
-
-Heute.21
 
 -->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Moonproject</title>
+        <title>Moonprojects</title>
     </head>
     <body>
 	Git Testing
+	
     </body>
 </html>
