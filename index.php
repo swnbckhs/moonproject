37650c458b141497343593868216ecba922f21b5
Dozent117 <Dozent117@figd-1611700.figdtk.de>
2017-02-21 13:05:19 +0100

    new project - init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+https://github.com/swnbckhs/moonproject.git
+
+swnbckhs
+
+Heute.21
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Moonproject</title>
+    </head>
+    <body>
+	Git Testing
+    </body>
+</html>
